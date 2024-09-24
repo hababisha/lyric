d034b7450b96bc7d9c1692768af9df9688d58d56
hababisha <haileamlakderbie1@gmail.com>
2024-09-24 21:20:36 +0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://mezmure.vercel.app", "http://localhost:5173"],
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+    
+));
 app.use(express.json());
 
 // MongoDB connection
--- frontend/src/components/Cards/LyricCard.jsx
+++ frontend/src/components/Cards/LyricCard.jsx
@@ -17,7 +17,7 @@ const LyricCard = ({ lyric }) => {
 
     return (
         <div 
-            className={`border rounded-md p-4 pb-20 bg-gray-800 transition-all duration-300 ${isExpanded ? 'h-auto' : 'h-20 overflow-hidden'} `}
+            className={`border rounded-md p-4 pb-20 bg-gray-800 transition-all duration-300 ${isExpanded ? 'h-auto' : 'h-20 overflow-hidden'}`}
             onClick={toggleExpand}
         >
             <div className="flex justify-between items-center">
@@ -25,12 +25,18 @@ const LyricCard = ({ lyric }) => {
                 <FaChevronDown className={`text-gray-100 transition-transform duration-300 ${isExpanded ? 'rotate-180' : ''}`} />
             </div>
             {isExpanded ? (
-                <div className="mt-2 ">
-                    <p className='text-white'>{body}</p> {/* Full lyrics */}
+                <div className="mt-2">
+                    {/* Replace line breaks with <br /> */}
+                    <p className='text-white'>
+                        {body.split('\n').map((line, index) => (
+                            <span key={index}>{line}<br /></span>
+                        ))}
+                    </p>
                 </div>
             ) : (
                 <p className="mt-2 text-gray-100">
-                    {body.slice(0, 15)}{body.length > 15 ? '...' : ''}
+                    {/* Show the first 15 characters, if longer add ellipsis */}
+                    {body.length > 15 ? `${body.slice(0, 15)}...` : body}
                 </p>
             )}
         </div>
