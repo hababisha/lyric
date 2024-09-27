3f77c94bdef77b253f72c4f1aaea22e960d9168a
hababisha <haileamlakderbie1@gmail.com>
2024-09-28 01:09:02 +0300

    dkl

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors(
     {
-        origin: ["https://mezmure.vercel.app", "http://localhost:5173"],
+        origin: '*',
         methods: ["GET", "POST"],
         credentials: true
     }
